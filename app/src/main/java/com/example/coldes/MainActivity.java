221359a09a290352071e81b1657dd8ba252605dd
Alexei <alevure@yandex.ru>
2020-03-24 15:13:46 +0400

    connect spinner with textviwer and work button

--- app/src/main/java/com/example/coldes/MainActivity.java
+++ app/src/main/java/com/example/coldes/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.coldes;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerColors;
+    private TextView textViewDescriprtionTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerColors = findViewById(R.id.spinnerColors);
+        textViewDescriprtionTemp = findViewById(R.id.textViewDescriptionTemp);
+    }
+
+    public void showDescription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        String description = getDescriptionPosition(position);
+        textViewDescriprtionTemp.setText(description);
+    }
+
+    private String getDescriptionPosition(int position) {
+        String [] description = getResources().getStringArray(R.array.description_of_temp);
+        return description[position];
     }
 }
