72079c322d224f13bc5d5c4471716ef449971403
Alexei <alevure@yandex.ru>
2020-03-26 12:44:49 +0400

    fix second activity(ReceivedMsgActivity)

--- app/src/main/java/com/example/coldes/MainActivity.java
+++ app/src/main/java/com/example/coldes/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.coldes;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         spinnerColors = findViewById(R.id.spinnerColors);
         textViewDescriprtionTemp = findViewById(R.id.textViewDescriptionTemp);
     }
@@ -31,5 +35,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickSendMsg(View view) {
+        Intent intent = new Intent(this, ReceivedMsgActivity.class);
+        startActivity(intent);
     }
 }
